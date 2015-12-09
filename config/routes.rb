4247e2a5de42de8db8cdd26977329f157b1071b4
Gus De Vita <gdevita@gmail.com>
2015-12-09 12:37:55 +0100

    places controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :places, only: [:index, :new, :show, :create, :update, :destroy]
+
   devise_for :users
   root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
